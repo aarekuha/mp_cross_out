5530b22329a63700f011beab871a3db9347ea9a0
Рекуха Александр Александрович <aarekuha@gmail.com>
2024-10-25 13:22:31 +0300

    Solution

--- app.py
+++ app.py
@@ -1,20 +1,53 @@
-from fastapi import FastAPI
-import threading
-import logging
-import json
 import sys
+import json
+import logging
+import logging.handlers
+import queue
 import random
 import string
+import threading
+from contextlib import suppress
+
+from fastapi import FastAPI
 
 app = FastAPI()
 
-# Configure logging to output to stderr
+# # Configure logging to output to stderr
 logger = logging.getLogger("json_logger")
 logger.setLevel(logging.INFO)
 handler = logging.StreamHandler(sys.stderr)
 formatter = logging.Formatter('%(message)s')  # Output only the message
 handler.setFormatter(formatter)
 logger.addHandler(handler)
+
+
+class CustomQueueHandler(logging.handlers.QueueHandler):
+    def enqueue(self, record):
+        with suppress(queue.Full):
+            return super().enqueue(record)
+
+
+def setup_queue_logging(log_queue):
+    """
+    Глобальная настройка логирования с использованием QueueHandler.
+    Все сообщения логов будут отправляться в очередь.
+    """
+    # Создаем QueueHandler и добавляем его к корневому логгеру
+    queue_handler = CustomQueueHandler(log_queue)
+    root_logger = logging.getLogger()
+
+    # Удаляем все другие обработчики у корневого логгера
+    # Это необходимо, чтобы избежать дублирования сообщений
+    global logger
+    for handler in logger.handlers:
+        logger.removeHandler(handler)
+
+    root_logger.setLevel(logging.DEBUG)  # Установите желаемый уровень логирования
+    root_logger.addHandler(queue_handler)
+    root_logger.propagate = True
+
+    logger = root_logger
+
 
 # Функция генерации большого JSON
 def _generate_large_json(num_fields=1000, field_length=20):
@@ -35,11 +68,15 @@ def generate_large_json():
         json_str = json.dumps(data)
         logger.info(json_str)
 
+
 # Start the background thread when the app starts
 @app.on_event("startup")
 def startup_event():
+    if getattr(app, "logger_queue", None):
+        setup_queue_logging(app.logger_queue)
     thread = threading.Thread(target=generate_large_json, daemon=True)
     thread.start()
+
 
 @app.get("/")
 def read_root():
--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,4 +1,19 @@
+import logging
+from logging.handlers import QueueListener
+from multiprocessing import Queue
+
+
+log_queue = Queue(maxsize=10_000)
+
 # gunicorn.conf.py
+def post_fork(server, worker):
+    worker.app.wsgi().logger_queue = log_queue
+
+
+stream_handler = logging.StreamHandler()
+queue_listener = QueueListener(log_queue, stream_handler)
+queue_listener.start()
+
 
 # Set the log level to critical to minimize output
 loglevel = 'critical'
@@ -9,7 +24,7 @@ errorlog = None
 # Do not capture output
 capture_output = False
 # Number of worker processes
-workers = 2
+workers = 4
 # Worker class
 worker_class = 'uvicorn.workers.UvicornWorker'
 
